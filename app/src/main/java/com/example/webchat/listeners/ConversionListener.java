b9c964858db0e606ca32ce5657463951b88b0232
ezJeeks <mopcuk2412@gmail.com>
2023-07-03 23:56:17 +0300

    done

---
+++ app/src/main/java/com/example/webchat/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.example.webchat.listeners;
+
+import com.example.webchat.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/example/webchat/listeners/UserListener.java
@@ -0,0 +1,8 @@
+package com.example.webchat.listeners;
+
+import com.example.webchat.models.User;
+
+public interface UserListener {
+
+    void onUserClicked(User user);
+}
